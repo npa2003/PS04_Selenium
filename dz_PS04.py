9baa0f33315c4d92cbad011dc9b6b9ad03153055
Pavel N <npa2003@gmail.com>
2025-03-25 20:54:25 +0300

    2

--- dz_PS04.py
+++ dz_PS04.py
@@ -6,11 +6,11 @@ from selenium.webdriver.common.by import By
 
 import time
 
-#search_text = input ('Введите текст для поиска:')
-search_text = 'мороженое'
+search_text = input ('Введите текст для поиска:')
+#search_text = 'мороженое'
 
 browser = webdriver.Firefox()
-browser.get('https://ru.wikipedia.org/')
+browser.get('https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0')
 time.sleep(1)
 assert 'Википедия' in browser.title
 
